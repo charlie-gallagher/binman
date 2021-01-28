7ba87d939c206abde8f96d3db7e7104c07cb84e9
Charlie Gallagher <charlesjgallagher15@gmail.com>
2021-01-28 12:42:56 -0500

    Update error style and add error to test.bat

--- src/bin_error.c
+++ src/bin_error.c
@@ -6,87 +6,86 @@ void print_errors(void) {
     extern int bin_errno;
     switch (bin_errno) {
         case CANNOT_OPEN_TMP: {
-            fprintf(stderr, "\nCannot open temporary file.\n");
+            fprintf(stderr, "\nError: Cannot open temporary file\n");
             break;
         }
         case CANNOT_OPEN: {
-            fprintf(stderr, "\nCannot open file.\n");
+            fprintf(stderr, "\nError: Cannot open file\n");
             break;
         }
         case CANNOT_OVERWRITE: {
-            fprintf(stderr, "\nCannot overwrite file.\n");
+            fprintf(stderr, "\nError: Cannot overwrite file\n");
             break;
         }
         case FAILED_TO_COPY: {
-            fprintf(stderr, "\nFailed to copy file to internal structure.\n");
+            fprintf(stderr, "\nError: Failed to copy file to internal structure\n");
             break;
         }
         case FILENOTEXIST: {
-            fprintf(stderr, "\nFile does not exist.\n");
+            fprintf(stderr, "\nError: File does not exist\n");
             break;
         }
         case ILLEGAL_ARGUMENT: {
-            fprintf(stderr, "\nIllegal argument.\n");
+            fprintf(stderr, "\nError: Illegal argument\n");
             break;
         }
         case INVALID_FLAG: {
-            fprintf(stderr, "\nInvalid flag.\n");
+            fprintf(stderr, "\nError: Invalid flag\n");
             break;
         }
         case INVALID_PARAM: {
-            fprintf(stderr, "\nInvalid parameter.\n");
+            fprintf(stderr, "\nError: Invalid parameter\n");
             break;
         }
         case INVALID_WORD: {
-            fprintf(stderr, "\nInvalid word size.\n");
+            fprintf(stderr, "\nError: Invalid word size\n");
             break;
         }
         case INTERLEAVE_AND_DEINTERLEAVE: {
-            fprintf(stderr, "\nCannot interleave and de-interleave in one operation.\n");
+            fprintf(stderr, "\nError: Cannot interleave and de-interleave in one operation\n");
             break;
         }
         case TWOIN_TWOOUT: {
-            fprintf(stderr, "\nCannot have two inputs and two outputs.\n");
+            fprintf(stderr, "\nError: Cannot have two inputs and two outputs\n");
             break;
         }
         case TOO_MANY_INPUT: {
-            fprintf(stderr, "\nToo many input files.\n");
+            fprintf(stderr, "\nError: Too many input files\n");
             break;
         }
         case TOO_FEW_INPUT: {
-            fprintf(stderr, "\nToo few input files.\n");
+            fprintf(stderr, "\nError: Too few input files\n");
             break;
         }
         case TOO_MANY_OUTPUT: {
-            fprintf(stderr, "\nToo many output files.\n");
+            fprintf(stderr, "\nError: Too many output files\n");
             break;
         }
         case TOO_FEW_OUTPUT: {
-            fprintf(stderr, "\nToo few output files.\n");
+            fprintf(stderr, "\nError: Too few output files\n");
             break;
         }
         case START_BYTE_AFTER_END: {
-            fprintf(stderr, "\nStart byte after end byte.\n");
+            fprintf(stderr, "\nError: Start byte after end byte\n");
             break;
         }
         case INVALID_OUT_TYPE: {
-            fprintf(stderr, "\nInvalid output type.\n");
+            fprintf(stderr, "\nError: Invalid output type\n");
             break;
         }
         case WRITE_ERROR: {
-            fprintf(stderr, "\nFailed to write to file.\n");
+            fprintf(stderr, "\nError: Failed to write to file\n");
             break;
         }
         case HELP_FILE_ERROR: {
-            fprintf(stderr, "\nError opening help file.\n");
+            fprintf(stderr, "\nError: Error opening help file\n");
             break;
         }
         case 0: {
-            fprintf(stderr, "No errors.\n");
             break;
         }
         default: {
-            fprintf(stderr, "Unknown error code.\n");
+            fprintf(stderr, "\nError: unrecognized error code\n");
             break;
         }
     }
